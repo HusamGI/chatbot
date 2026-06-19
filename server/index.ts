9da8efa47a89a61e9b55bb88908dc2387fc6980e
Husam Ahmed <des-techno@hotmail.com>
2026-06-19 21:20:22 +0300

    added conversations history

--- server/index.ts
+++ server/index.ts
@@ -21,15 +21,21 @@ app.get('/api/hello', (_request: Request, response: Response) => {
   response.json({ message: 'Hello World!' });
 });
 
+// In a real scenario this conversation map should be saved in the database
+const conversations = new Map<string, string>();
+
 app.post('/api/chat', async (request: Request, response: Response) => {
-  const { prompt } = request.body;
+  const { prompt, conversationId } = request.body;
 
   const gptResponse = await client.responses.create({
     model: 'gpt-4o-mini',
     input: prompt,
     temperature: 0.2,
     max_output_tokens: 100,
+    previous_response_id: conversations.get(conversationId),
   });
+
+  conversations.set(conversationId, gptResponse.id);
 
   response.json({ message: gptResponse.output_text });
 });

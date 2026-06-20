8874968c59ed05c1caffdb944c517fba0976a4d4
Husam Ahmed <des-techno@hotmail.com>
2026-06-20 11:41:59 +0300

    refactoring, added routes

---
+++ packages/server/routes.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import type { Request, Response } from 'express';
+import { chatController } from './controllers/chat.controller';
+
+const router = express.Router();
+
+router.get('/', (_request: Request, response: Response) => {
+  response.send('Hello World!');
+});
+
+router.get('/api/hello', (_request: Request, response: Response) => {
+  response.json({ message: 'Hello World!' });
+});
+
+router.post('/api/chat', chatController.sendMessage);
+
+export default router;
